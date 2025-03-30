de75b40a26cd50b046723566b13ee654b349c56b
Glennmark2002 <glennmark.culibra@gmail.com>
2025-03-30 13:15:23 +0800

    asdasdad

--- client/src/components/QRScanner.jsx
+++ client/src/components/QRScanner.jsx
@@ -24,9 +24,26 @@ const QRScanner = ({ onScan }) => {
     };
   }, [onScan]);
 
-  return <div>
-      <video ref={videoRef} className='w-full' />;
+  return (
+    <div className="relative w-full max-w-md mx-auto">
+      {/* QR Scanner Video */}
+      <video ref={videoRef} className="w-full rounded-lg" />
+
+      {/* Scanner Overlay */}
+      <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
+        <div className="border-4 border-blue-500 w-40 h-40 relative">
+          {/* Top Left Corner */}
+          <div className="absolute top-0 left-0 w-6 h-6 border-t-4 border-l-4 border-white"></div>
+          {/* Top Right Corner */}
+          <div className="absolute top-0 right-0 w-6 h-6 border-t-4 border-r-4 border-white"></div>
+          {/* Bottom Left Corner */}
+          <div className="absolute bottom-0 left-0 w-6 h-6 border-b-4 border-l-4 border-white"></div>
+          {/* Bottom Right Corner */}
+          <div className="absolute bottom-0 right-0 w-6 h-6 border-b-4 border-r-4 border-white"></div>
+        </div>
+      </div>
     </div>
+  );
 };
 
 export default QRScanner;
