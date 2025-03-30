fa62504c69b8ce358b6acf4abd0573f4e8378f36
Glennmark2002 <glennmark.culibra@gmail.com>
2025-03-30 13:20:23 +0800

    overlay

--- client/src/components/QRScanner.jsx
+++ client/src/components/QRScanner.jsx
@@ -29,16 +29,17 @@ const QRScanner = ({ onScan }) => {
       {/* QR Scanner Video */}
       <video ref={videoRef} className="w-full rounded-lg" />
 
-      {/* Scanner Overlay */}
-      <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
-        <div className="border-4 border-blue-500 w-40 h-40 relative">
-          {/* Top Left Corner */}
+      {/* Dark Overlay */}
+      <div className="absolute inset-0 bg-black bg-opacity-70 flex items-center justify-center">
+        {/* Scanner Frame */}
+        <div className="relative w-40 h-40 border-4 border-blue-500 bg-transparent">
+          {/* Cutout effect */}
+          <div className="absolute inset-0 bg-black bg-opacity-70"></div>
+
+          {/* Corners */}
           <div className="absolute top-0 left-0 w-6 h-6 border-t-4 border-l-4 border-white"></div>
-          {/* Top Right Corner */}
           <div className="absolute top-0 right-0 w-6 h-6 border-t-4 border-r-4 border-white"></div>
-          {/* Bottom Left Corner */}
           <div className="absolute bottom-0 left-0 w-6 h-6 border-b-4 border-l-4 border-white"></div>
-          {/* Bottom Right Corner */}
           <div className="absolute bottom-0 right-0 w-6 h-6 border-b-4 border-r-4 border-white"></div>
         </div>
       </div>
